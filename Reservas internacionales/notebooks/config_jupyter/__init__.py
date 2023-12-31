b68ddbbd21f7aadb9e5b154001e3e1960c96deca
Martín Basualdo <70729951+MartinBasualdo0@users.noreply.github.com>
2023-12-31 00:04:16 -0300

    reacomodamiento reservas internacionales

--- Reservas internacionales/notebooks/config_jupyter/__init__.py
+++
@@ -1,1 +0,0 @@
-from .config import set_wd
--- Reservas internacionales/notebooks/config_jupyter/config.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-def set_wd():
-    sys.path.append(os.path.abspath('../'))
-    dir = os.getcwd()
-    os.chdir(dir.replace("\\jupyter", ""))
-    return os.getcwd()
